fe88702afcb565586c0dfb735c277cc3c0517eb0
animeshSDE <animesh.anand@swisstech.in>
2025-08-23 23:45:26 +0530

    added testimonials component

---
+++ src/components/Testimonials/Testimonials.jsx
@@ -0,0 +1,12 @@
+// Testimonials component
+import styles from './Testimonials.module.css';
+
+const Testimonials = () => {
+  return (
+    <section className={styles.testimonials}>
+      <h2>Testimonials</h2>
+    </section>
+  );
+};
+
+export default Testimonials;
