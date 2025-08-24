bc057a797061b2c1ef549c1b37cd9d4bc2b38a6e
animeshSDE <animesh.anand@swisstech.in>
2025-08-24 19:50:08 +0530

    update homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../../components/Hero/Hero";
 import SecondaryHero from "../../components/Hero/SecondaryHero";
+import HotelListings from "../../components/HotelListings/HotelListings";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
       <SecondaryHero />
+      <HotelListings/>
       {/* <Testimonials /> */}
       {/* etc... */}
     </>
